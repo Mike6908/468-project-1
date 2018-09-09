7fc353059a4c1030fd71f6af2c221d9bedd95ae8
Mike6908 <mlong6908@hotmail.com>
2018-09-09 10:11:34 -0400

    First Commit for 486 Project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    constructor(){
+        super();
+    }
+    render(){
+    
+        return (
+                <div>
+                    Yo World
+                </div>
+            );
+    }
+}
+
+export default App;
